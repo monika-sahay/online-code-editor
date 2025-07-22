218b4076e8c36705ccca74c135b7e4b26a463d36
CodeSandbox <support@codesandbox.io>
2025-07-22 12:53:23 +0000

    add varcel app to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,12 @@ const nextConfig = {
     allowedDevOrigins: [
       "https://2jfjkj-8000.csb.app",
       "http://localhost:8000",
+      "http://localhost:8001",
       "http://localhost:3000",
       "http://127.0.0.1:8000",
-      "http://127.0.0.1:3000"
+      "http://127.0.0.1:3000",
+      "http://172.17.0.2:8000",
+      "https://online-code-editor-nine-chi.vercel.app",
     ],
   images: {
     remotePatterns: [
